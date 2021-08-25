5753f0beabbaf8f135a375d5af2276fd70398120
Jihoon <min@iiasa.ac.at>
2021-08-25 09:56:13 +0200

    Make initial Gini trajectories

---
+++ Code/0.background_data.R
@@ -0,0 +1,108 @@
+library(tidyverse)
+library(readxl)
+library(countrycode)
+library(units)
+library(WDI)
+library(scales)
+
+data.path =  "./data/"
+
+# Standard ISO3 for "countries"
+iso3 = setdiff(countrycode::codelist$iso3c, NA)
+
+# WB thresholds per Country group (2020)
+# Source: https://blogs.worldbank.org/opendata/new-world-bank-country-classifications-income-level-2021-2022
+thres = c(b = 0, l = 1046, m = 4096, h = 12695, Inf) # GNI breaks for group classification
+povline = c(LIC = 1.9, LMIC = 3.2, UMIC = 5.5, HIC = 21.7)
+
+# Set up 2020 base year GNI/GDP data from WDI
+gni2020 = WDI(indicator =c("NY.GNP.PCAP.CD", # GNI per capita, Atlas method (current US$)
+                           # "NY.GNP.PCAP.KD", # GNI per capita, PPP (constant 2017 international $)
+                           
+                           "NY.GDP.PCAP.PP.CD", # GDP per capita, PPP (current international $)
+                           "NY.GDP.PCAP.PP.KD"  # GDP per capita, PPP (constant 2017 international $)
+                           # "NE.CON.PRVT.PC.KD"  # GDP per capita, PPP (constant 2017 international $)
+                           ), start = 2010, end=2020, extra=TRUE) %>% 
+  rename(gni = NY.GNP.PCAP.CD) %>%
+  filter(!is.na(iso3c), region!="Aggregates", year %in% c(2019, 2020), !is.na(gni)) %>%  # some countries without 2020 GNI
+  select(-c(capital:latitude, lending, iso2c)) %>% arrange(iso3c, -year) %>%
+  group_by(country, iso3c) %>%
+  summarise(across(gni:NY.GDP.PCAP.PP.KD, mean)) %>% ungroup() %>%
+  
+  mutate(inc.grp = cut(gni, breaks=thres, labels=FALSE)) %>% # Numeric poverty lines
+  group_by(inc.grp) %>%
+  mutate(min = min(gni), med = median(gni), max = max(gni)) %>%  # min/med/max of the GNI within each group
+  mutate(grp.lower = pmax(inc.grp-1, 1)) %>% # The group below
+  mutate(pov0 = povline[inc.grp], pov.low = povline[grp.lower]) %>%
+  mutate(povline.adj = pmin((pov0-pov.low)/(med-min) * (gni-min) + pov.low, pov0)) %>%
+  mutate(gdp.gni.ratio = NY.GDP.PCAP.PP.KD/gni) # Between 2017 PPP$ and current Atlas
+
+# Gini WDI avg (2015-)
+gini.wb = WDI(indicator="SI.POV.GINI", start = 2015, extra=TRUE) %>% 
+  filter(iso3c %in% iso3) %>% arrange(iso3c, year) %>% 
+  group_by(country, iso3c) %>%
+  summarise(gini.baseyr = mean(SI.POV.GINI, na.rm = TRUE)) %>%
+  drop_na()
+
+# GDP conversion between 2005$ PPP and 2017$ PPP
+ppp.conv = WDI(indicator = c("PA.NUS.PPP", "NY.GDP.DEFL.ZS"), start = 2005, end=2017, extra=TRUE) %>%
+  filter(year %in% c(2005, 2011, 2017), iso3c %in% iso3) %>% 
+  pivot_wider(names_from = year, values_from = c(PA.NUS.PPP, NY.GDP.DEFL.ZS)) %>% 
+  mutate(ppp.2005.to.2017 = PA.NUS.PPP_2005*NY.GDP.DEFL.ZS_2017/NY.GDP.DEFL.ZS_2005/PA.NUS.PPP_2017) %>%
+  mutate(ppp.2017.to.2011 = PA.NUS.PPP_2017*NY.GDP.DEFL.ZS_2011/NY.GDP.DEFL.ZS_2017/PA.NUS.PPP_2011) %>%
+  select(iso3c, ppp.2005.to.2017, ppp.2017.to.2011)
+
+# Households and NPISHs final consumption expenditure (% of GDP)
+# Get the latest non-NA value, based on the assumption that it's not varying fast.
+# Note: Forget NPISH, which is only reported in EU countries
+fin.con = WDI(indicator ="NE.CON.PRVT.ZS", latest=1, extra=TRUE) %>%  
+  rename(final.cons.rate = NE.CON.PRVT.ZS) %>% mutate(final.cons.rate = final.cons.rate/100) %>%
+  filter(!is.na(iso3c), region!="Aggregates", year > 2000) %>% 
+  select(-c(capital:latitude, lending, iso2c)) %>% arrange(iso3c, -year) %>%
+  select(iso3c, final.cons.rate)
+
+# Passthrough rate taken from Lakner et al. 2019
+passthrough = 0.83
+
+# Historical GDP/GNI vs poverty line by country (2011$ PPP or Atlas (GNI))
+hist = read_xlsx(paste0(data.path, "Historical_poverty_lines_WB.xlsx"), skip=5) %>%
+  mutate(ln.NPL = log(NPL), ln.GNI = log(GNI)) %>%
+  mutate(ln.NPL.normalized = (ln.NPL - min(ln.NPL))/(max(ln.NPL) - min(ln.NPL)))
+
+# Estimate different S-ish models
+model1 <- glm(ln.NPL.normalized ~ ln.GNI, family=binomial(link='logit'), data=hist)
+model2 <- nls(ln.NPL ~ a*exp(-b*exp(- c*ln.GNI)), data=hist, start=list(a=50, b=1, c=3)) # Gomperzt (Sigmoid)
+# model2 <- nls(ln.NPL ~ a/(1 + exp(-b * (ln.GNI-c)) ), data=hist, start=list(a=5, b=1, c=3))
+
+# Fitted curves
+fitted.results1 <- predict(model1, newdata=hist, type='response')
+fitted.results2 <- predict(model2, newdata=hist, type='response') 
+
+# Plot obs and predictions
+df.test = hist %>% 
+  mutate(ln.fit1 = fitted.results1 * (max(ln.NPL) - min(ln.NPL)) + min(ln.NPL)) %>%
+  mutate(ln.fit2 = fitted.results2) %>%
+  mutate(fit1 = exp(ln.fit1), fit2 = exp(ln.fit2))
+ggplot(data = df.test) +
+  geom_point(aes(GNI, NPL)) + # Observations
+  scale_x_continuous(trans='log', breaks=scales::trans_breaks("log", function(x) 2^x)) +
+  scale_y_continuous(trans='log', breaks=scales::trans_breaks("log", function(x) 2^x)) +
+  geom_line(aes(GNI, fit1, color='Logistic'), size=1.5) +
+  geom_line(aes(GNI, fit2, color='Gompertz'), size=1.5) +
+  theme_bw() +
+  labs(x="GNI per capita per day (2011 Atlas USD)", y="National poverty line per day (2011 PPP)")
+
+# Test plot to observe the behavior
+gni.test = seq(1, 3600, 1)
+pred1 = predict(model1, newdata=data.frame(GNI = gni.test, ln.GNI=log(gni.test)), type='response')
+pred1 = exp(pred1 * (max(hist$ln.NPL) - min(hist$ln.NPL)) + min(hist$ln.NPL))
+pred2 = exp(predict(model2, newdata=data.frame(ln.GNI=log(gni.test)), type='response'))
+
+df.plot = data.frame(g=gni.test,n=pred1) %>%
+  mutate(ln.g = log(g), ln.n = log(n))
+ggplot(data=df.plot) + geom_line(aes(x=g, y=n))+ 
+  scale_x_continuous(trans='log') +
+  scale_y_continuous(trans='log')
+
+# Model2 makes more sense in that the richer will get more demanding NPL.
+
--- Code/1.set_threshold_trajectories.R
+++ Code/1.set_threshold_trajectories.R
@@ -1,73 +1,179 @@
-library(tidyverse)
-library(readxl)
-library(countrycode)
-library(units)
-library(WDI)
-
-data.path =  "./data/"
-
-# ISO3 for "countries"
-iso3 = setdiff(countrycode::codelist$iso3c, NA)
-
-# WB thresholds per Country group (2020)
-# Source: https://blogs.worldbank.org/opendata/new-world-bank-country-classifications-income-level-2021-2022
-thres = c(b = 0, l = 1046, m = 4096, h = 12695, Inf) # GNI breaks for group classification
-povline = c(LIC = 1.9, LMIC = 3.2, UMIC = 5.5, HIC = 21.7)
-
-gni2020 = WDI(indicator =c("NY.GNP.PCAP.CD", # GNI per capita, PPP (current international $)
-                           "NY.GNP.PCAP.KD", # GNI per capita, PPP (constant 2017 international $)
-                           "NY.GDP.PCAP.CD", # GDP per capita, PPP (current international $)
-                           "NY.GDP.PCAP.KD"  # GDP per capita, PPP (constant 2017 international $)
-                           ), start = 2010, end=2020, extra=TRUE) %>% 
-  rename(gni = NY.GNP.PCAP.CD) %>%
-  filter(!is.na(iso3c), region!="Aggregates", year == 2020, !is.na(gni)) %>% 
-  select(-c(capital:latitude, lending, iso2c)) %>% arrange(iso3c, -year) %>%
-  mutate(inc.grp = cut(gni, breaks=thres, labels=FALSE)) %>% # Numeric poverty lines
-  group_by(inc.grp) %>%
-  mutate(min = min(gni), med = median(gni), max = max(gni)) %>%  # min/med/max of the GNI within each group
-  mutate(grp.lower = pmax(inc.grp-1, 1)) %>% # The group below
-  mutate(pov0 = povline[inc.grp], pov.low = povline[grp.lower]) %>%
-  mutate(povline.adj = pmin((pov0-pov.low)/(med-min) * (gni-min) + pov.low, pov0)) %>%
-  mutate(gdp.gni.const = NY.GDP.PCAP.KD/NY.GNP.PCAP.KD, gdp.gni.current = NY.GDP.PCAP.CD/gni)
+#### Income group identification ####
 
 # Income group characteristics based on 2020 categories
 groups.2020 <- gni2020 %>% select(inc.grp, min, med, max) %>% unique()
 names(groups.2020)[-1] <- paste0(names(groups.2020)[-1] ,'.2020')
 
+# Test plot for threshold trajectory
 ggplot(gni2020) +
   geom_point(aes(x = gni/365, y = povline.adj)) + 
   scale_x_continuous(trans='log') + 
   scale_y_continuous(trans='log')
 
+
+
+#### Load SDP scenario data ####
+
+# Read in PIK data on GDP and population, and derive GDP/cap
+# Convert 2005$ PPP to 2017$ PPP (as in WDI)
 gdp_data = read_delim(paste0(data.path, "release_v1p0/SHAPE_GDP_v1p0.mif"), delim=';') %>% select(-X25) %>%
   pivot_longer(cols = `2010`:`2100`, names_to = 'Year', values_to = 'GDP.bil') %>% select(-Variable, -Unit)
-
 pop_data = read_delim(paste0(data.path, "release_v1p0/SHAPE_POP_v1p0.mif"), delim=';') %>% select(-X25) %>%
   pivot_longer(cols = `2010`:`2100`, names_to = 'Year', values_to = 'POP.mil') %>% select(-Model, -Scenario, -Variable, -Unit)
+gdp_pcap = gdp_data %>% left_join(pop_data, by = c("Region", "Year")) %>% rename(iso3c=Region) %>%
+  left_join(ppp.conv) %>% mutate(GDP.bil = GDP.bil*ppp.2005.to.2017)%>%
+  mutate(gdp.pcap = GDP.bil/POP.mil*1000) 
 
-gdp_pcap = gdp_data %>% left_join(pop_data, by = c("Region", "Year")) %>%
-  mutate(gdp.pcap = GDP.bil/POP.mil*1000) %>% rename(iso3c=Region)
+df.gr = gdp_pcap %>% mutate(GDP.pcap = GDP.bil/POP.mil) %>% group_by(iso3c, Scenario) %>%
+  mutate(GDP.pcap.pre = lag(GDP.pcap)) %>% mutate(gr.r = (GDP.pcap/GDP.pcap.pre)^0.2 -1) # Annual avg GDP growth rate
 
-# Households and NPISHs final consumption expenditure (% of GDP)
-# Get the latest non-NA value, based on the assumption that it's not varying fast.
-fin.con = WDI(indicator ="NE.CON.PRVT.ZS", latest=1, extra=TRUE) %>% 
-  rename(final.cons.rate = NE.CON.PRVT.ZS) %>% mutate(final.cons.rate = final.cons.rate/100) %>%
-  filter(!is.na(iso3c), region!="Aggregates", year > 2000) %>% 
-  select(-c(capital:latitude, lending, iso2c)) %>% arrange(iso3c, -year) %>%
-  select(iso3c, final.cons.rate)
 
+#### Organize data for further analysis ####
+
+# Add HH expenditure per capita derived based on the latest share (NE.CON.PRVT.ZS)
 master = gni2020 %>% left_join(fin.con) %>%
-  mutate(povline.gdp = povline.adj/final.cons.rate) # in 2011 PPP, same as the original WB poverty line
+  # mutate(povline.gdp = povline.adj/final.cons.rate) %>% # in 2011 PPP, same as the original WB poverty line
+  # mutate(hh.exp.pcap.CD = NY.GDP.PCAP.CD*final.cons.rate) %>%
+  mutate(hh.exp.pcap.KD = NY.GDP.PCAP.PP.KD * final.cons.rate) %>% # in 2017$ PPP
+  left_join(ppp.conv) %>%
+  mutate(hh.exp.pcap.avg.2020 = hh.exp.pcap.KD * ppp.2017.to.2011) %>%# in 2011$ PPP
 
-a = master %>% ungroup() %>% select(country, gni, povline.adj, gdp.gni.current, iso3c) %>% right_join(gdp_pcap, by="iso3c") %>%
-  rename(gni.2020 = gni, povline.2020 = povline.adj) %>%
-  mutate(GNI.bil = GDP.bil / gdp.gni.current) %>%
+# Derive step-like thresholds with slopes (based on Elmar's suggestion)
+  rename(gni.2020 = gni) %>% ungroup() %>% select(country, gni.2020, gdp.gni.ratio, iso3c, hh.exp.pcap.avg.2020) %>% right_join(gdp_pcap, by="iso3c") %>%
+
+  mutate(GNI.bil = GDP.bil / gdp.gni.ratio) %>%
   mutate(gni.pcap = GNI.bil / POP.mil * 1000) %>%
   mutate(inc.grp = cut(gni.pcap, breaks=thres, labels=FALSE)) %>%
   group_by(inc.grp) %>%
   mutate(min = min(gni.pcap), med = median(gni.pcap), max = max(gni.pcap)) %>%  # min/med/max of the GNI within each group
   left_join(groups.2020) %>%
   mutate(grp.lower = pmax(inc.grp-1, 1)) %>% # The group below
-  mutate(povline0 = povline[inc.grp], povline.lower = povline[grp.lower]) %>%
-  mutate(povline.adj = pmin((povline0-povline.lower)/(med-min) * (gni.pcap-min) + povline.lower, povline0)) 
+  mutate(povline0 = povline[inc.grp], povline.lower = povline[grp.lower]) %>% # povline0: reference pov line given the GNI
+  mutate(povline.adj = pmin((povline0-povline.lower)/(med-min) * (gni.pcap-min) + povline.lower, povline0)) # Pov line with slopes
+
+# Derive avg HH exp projections from GDP trajectories by scenario
+# Unit: HH and GDP in 2017$ PPP, GNI in Atlas current
+df = master %>% 
+  select(-c(Model, gni.2020, GDP.bil, GNI.bil, gdp.gni.ratio, ppp.2005.to.2017, grp.lower, povline.lower)) %>%
+  drop_na() %>% 
+  filter(Year >= 2020) %>%
+  group_by(iso3c, Scenario) %>%
+  mutate(gdp.pcap.pre = lag(gdp.pcap)) %>%
+  mutate(gr.gdp = gdp.pcap/gdp.pcap.pre - 1) %>%
+  replace_na(replace = list(gr.gdp = 0)) %>%
+  mutate(gr.hh.exp = gr.gdp * passthrough + 1, gr.test = (passthrough*((1+gr.gdp)^0.2-1)+1)^5) %>% # HH expenditure growth rate: The two are close values.
+  mutate(hh.idx = cumprod(gr.test)) %>%
+  mutate(hh.exp.pcap.avg = hh.exp.pcap.avg.2020 * hh.idx) %>% # in 2011$ PPP
+  mutate(hh.exp.pcap.avg.day = hh.exp.pcap.avg / 365)
+
+
+
+#### Derive Gini trajectories ####
+
+gini.lbound = 20 # Arbitrary Gini lower bound at 20
+
+# Estimate NPI target based on GNI/day
+traj = df %>% select(-c(hh.exp.pcap.avg.2020, gdp.pcap, inc.grp:max.2020, gdp.pcap.pre:hh.exp.pcap.avg)) %>%
+  left_join(gini.wb) %>%
+  mutate(gni.day = gni.pcap/365) %>% select(-gni.pcap)%>%
+  mutate(ln.GNI = log(gni.day)) %>%
+  ungroup() %>%
+  # mutate(ln.NPI = predict(model2, newdata=traj, type='response')) %>%
+  # muate(ln.NPI = exp(ln.NPI * (max(hist$ln.NPL) - min(hist$ln.NPL)) + min(hist$ln.NPL))
+  mutate(ln.NPI = predict(model1, newdata=traj, type='response') * 
+           (max(hist$ln.NPL) - min(hist$ln.NPL)) + min(hist$ln.NPL)  ) %>%
+  mutate(povline.trend = exp(ln.NPI)) %>%
+  
+  # Target gini aiming for WB pov lines
+  mutate(gini.tgt.povl = gini.baseyr * (hh.exp.pcap.avg.day - povline0) / hh.exp.pcap.avg.day) %>%
+  mutate(gini.floor = gini.baseyr * 0.96^(as.numeric(Year) - 2020)) %>% # -4% maximum decrease (Lakner et al.)
+  mutate(achieved.povl = (gini.floor < gini.tgt.povl) & (gini.tgt.povl > gini.lbound)) %>% 
+  # Target gini aiming for the S-trend curve (arbitrary asymtote (current max))
+  mutate(gini.tgt.trend = gini.baseyr * (hh.exp.pcap.avg.day - povline.trend) / hh.exp.pcap.avg.day) %>%
+  mutate(achieved.trend = (gini.floor < gini.tgt.trend) & (gini.tgt.trend > gini.lbound)) %>% 
+  
+  mutate(check = (achieved.trend==achieved.povl))
+
+# Attempt to construct Gini path when on-track
+# traj.sub = traj %>% filter(!is.na(gini.baseyr)) %>%
+#   group_by(iso3c, Scenario) %>%
+#   mutate(povline.pre = lag(povline.trend), hh.exp.pre = lag(hh.exp.pcap.avg.day)) %>%
+#   mutate(scaler = (hh.exp.pcap.avg.day - povline.trend)/(hh.exp.pre - povline.pre) * hh.exp.pre/hh.exp.pcap.avg.day) %>% #gini scaler = k*mu_x/mu_z
+#   # mutate(gini.traj = gini.tgt.trend * scaler) %>%
+#   replace_na(replace = list(scaler = 1)) %>%
+#   mutate(sc.cumul = cumprod(scaler))
+ 
+# When each country meets the target (trend line)
+hc0 = traj %>%
+  group_by(iso3c, Scenario, achieved.trend) %>%
+  summarise(Year = first(Year), gini.hc0 = first(gini.tgt.trend)) %>%
+  filter(achieved.trend == TRUE) %>% # Identify the year when the headcount=0 is achieved.
+  rename(year.achieved = Year) %>% select(-achieved.trend)
+
+df.traj = traj %>% left_join(hc0) %>% drop_na(year.achieved) %>% filter(!is.na(gini.baseyr)) %>%
+  mutate(ontrack = ifelse(Year<year.achieved, -1, ifelse(Year==year.achieved, 0, 1)))
+# df.traj.all = traj %>% left_join(hc0) %>% filter(!is.na(gini.baseyr)) %>%
+#   mutate(ontrack = ifelse(Year<year.achieved, -1, ifelse(Year==year.achieved, 0, 1)))
+
+# Gini path when on-track
+a = df.traj %>% filter(ontrack>=0) %>% 
+  group_by(iso3c, Scenario) %>%
+  mutate(povline.pre = lag(povline.trend), hh.exp.pre = lag(hh.exp.pcap.avg.day)) %>%
+  mutate(scaler = (hh.exp.pcap.avg.day - povline.trend)/(hh.exp.pre - povline.pre) * hh.exp.pre/hh.exp.pcap.avg.day) %>% #gini scaler = k*mu_x/mu_z
+  replace_na(replace = list(scaler = 1)) %>%
+  mutate(sc.cumul = cumprod(scaler)) %>%
+  mutate(gini.traj = gini.hc0 * sc.cumul)
+
+# Gini path when off-track
+b = df.traj %>% filter(ontrack<=0) %>% #ungroup() %>%
+  add_count(iso3c, Scenario) %>%
+  group_by(iso3c, Scenario) %>%
+  mutate(gini.traj = seq(first(gini.baseyr), first(gini.hc0), length.out=first(n))) %>%
+  select(-n)
+
+# Combined Gini trajectories
+df.gini = rbind(a, b) %>% arrange(country, Scenario, Year) %>%
+  select(country:POP.mil, contains("povl"), contains("gini"), contains("hh.exp.pcap"), -contains("pre"), ontrack) %>%
+  unique(.) %>%
+  left_join(df.gr %>% select(Scenario:Year, gr.r))
+df.ontrack = df.gini %>% filter(ontrack==0) %>% 
+  select(country:Year, povline.achieved=povline.trend, avg.exp=hh.exp.pcap.avg.day, gini.traj)
+
+# Test plot
+l.size = 1.5
+
+cty = "AUT"
+ggplot(data=df.gini %>% filter(iso3c %in% c(cty)), aes(x=Year)) +
+  geom_line(aes(y=gini.traj, group=interaction(country, Scenario), color=Scenario), size=l.size)  +
+  geom_line(data=df.gr %>% filter(iso3c %in% c(cty)), aes(y=gr.r*1000, group=interaction(iso3c, Scenario), color=Scenario), linetype = "dashed", size=l.size) + 
+  geom_hline(yintercept=gini.lbound, linetype="dashed", color = "blue") +
+  geom_text(data=df.ontrack %>% 
+              filter(iso3c %in% c(cty),
+                     Scenario=="innovation"), 
+            aes(x=Year, y=gini.traj, 
+                label=paste0("Avg:", format(avg.exp, digits = 3), ", Min.:", format(povline.achieved, digits = 3), "($/day)")),
+            nudge_y = -2) +
+  geom_text(data=df.gini %>% 
+              filter(Year==2020, iso3c %in% c(cty),
+                     Scenario=="innovation"), 
+            aes(x=Year, y=gini.traj, 
+                label=paste0("Avg:", format(hh.exp.pcap.avg.day, digits = 3), "($/day)")),
+            nudge_y = -2) +
+  scale_y_continuous(
+    
+    # Features of the first axis
+    name = "Gini",
+    
+    # Add a second axis and specify its features
+    sec.axis = sec_axis(~./1000, name="GDP growth rate")
+  ) +
+  labs(title=cty) +
+  theme_bw()
+  
+
+
+
+# Observation
+# Rich countries (e.g. AUT) never meets the trend line goal, because it grows coutinuously.
+# TODO: test with the logistic function target
 
